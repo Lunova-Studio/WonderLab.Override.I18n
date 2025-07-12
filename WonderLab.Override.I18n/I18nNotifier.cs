3c19b2ae4f680cd42e11c0b16d1227d45f5916c3
YangSpring429 <3424968114@qq.com>
2025-07-13 00:17:45 +0800

    重写 I18n 绑定逻辑

---
+++ WonderLab.Override.I18n/I18nNotifier.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace WonderLab.Override.I18n;
+
+public sealed class I18nNotifier : INotifyPropertyChanged {
+    public static I18nNotifier Instance { get; } = new();
+    public static string Source => ""; // dummy value
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void Trigger() =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Source)));
+}
